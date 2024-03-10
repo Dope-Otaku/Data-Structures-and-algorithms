9f49fa86c1abe1523fa03031c0f35ef9a608d1ca
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-03-10 21:19:54 +0530

    added the method of insert after and insert at last with is_empty method

--- Singly Linked List/code.py
+++ Singly Linked List/code.py
@@ -1,8 +1,41 @@
+# singly linked list
+'''
+   created this node class where it has two items one is item(data) and another is next which points to next item 
+'''
 class Node:
     def __init__(self, item=None, next=None):
         self.item = item
         self.next = next
 
 class Singly_list:
-    def __init__(self, start):
+    '''
+    created an empty sll with a head where there is no data but only address pointer is saved as self.start
+    '''
+    def __init__(self, start=None):
         self.start = start
+    '''
+    created a method to check that if the list is empty or not
+    '''
+    def is_empty(self):
+        return self.start==None
+    '''
+    created an insert at start where we need to add a node between the head and and a node 
+    '''
+    def insert_at_start(self, data):
+        n = Node(data, self.start)
+        self.start = n
+    
+    def insert_at_last(self, data):
+        n = Node(data, None)
+        if not self.is_empty():
+            temp = self.start
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = n 
+        else:
+            self.start = n
+
+# driver code
+mylist = Singly_list()
+
+print(mylist.is_empty())

1c1ff82346df3b0d88ed76fc1120f2dde42b6852
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-03-10 19:25:46 +0530

    added the basic node class of sll

---
+++ Singly Linked List/code.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self, item=None, next=None):
+        self.item = item
+        self.next = next
+
+class Singly_list:
+    def __init__(self, start):
+        self.start = start

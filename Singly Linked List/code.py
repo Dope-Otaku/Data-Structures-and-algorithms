f1a9194d2aad01d253f81b2e2517ed5385107a7d
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-03-19 23:42:48 +0530

    learned insert between

--- Singly Linked List/code.py
+++ Singly Linked List/code.py
@@ -35,6 +35,8 @@
 #         else:
 #             self.start = n
 
+
+
 #     def search(self, data):
 #         if not self.is_empty():
 #             temp = self.start
@@ -57,33 +59,80 @@
 
 
 
-# class Node:
-#     def __init__(self, item=None, next=None):
-#         self.item = item
-#         self.next = next
+class Node:
+    def __init__(self, item=None, next=None):
+        self.item = item
+        self.next = next
 
-# class Sll:
-#     def __init__(self, start=None):
-#         self.start = start
+class Sll:
+    def __init__(self, start=None):
+        self.start = start
 
-#     def isEmpty(self):
-#         if self.start == None:
-#             return f"The singly linked list is empty"
-#         else:
-#             return f"the sll is not empty"
+    def isEmpty(self):
+        if self.start == None:
+            return f"The singly linked list is empty"
+        else:
+            return f"the sll is not empty"
         
     
         
-#     def insert_at_first(self, data):
-#         try: 
-#             node = Node(data, self.start)
-#             self.item = node
-#             print("node added at first")
-#         except:
-#             return f"failed to add a node at first"
+    def insert_at_first(self, data):
+        try: 
+            node = Node(data, self.start)
+            self.start = node  # Update the start of the list to the new node
+            print("node added at first")
+        except:
+            return f"failed to add a node at first"
+        
+    def insert_at_last(self, data):
+        node = Node(data)
+        if not self.isEmpty():
+            temp = self.start
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = node
+        else:
+            self.start = node 
+
+    def search(self, value):
+        if not self.isEmpty():
+            temp = self.start
+            while temp is not None:
+                if temp.item == value:
+                    f"found at {temp}"
+                else:
+                    temp = temp.next
+        else:
+            return f"list is empty"
+
+    def insertBefore(self, newdata, olddata):
+        if not self.isEmpty():
+            temp = self.start
+            prev = None
+            NewNode = Node(newdata)
+            while temp is not None:
+                if temp.item == olddata:
+                    if prev is None:
+                        NewNode.next = temp
+                        self.start = NewNode
+                    else:
+                        prev.next = NewNode
+                        NewNode.next = temp
+                    return f"Inserted {newdata} before {olddata}"
+                prev = temp
+                temp = temp.next
+            return f"Item {olddata} not found in the list"
+        else:
+            return f"List is empty"
+
+        
+
         
     
 
-# ll =  Sll()
-# print(ll.insert_at_first(2))
-# print(ll.isEmpty())
+ll =  Sll()
+print(ll.insert_at_first(2))
+print(ll.isEmpty())
+print(ll.insert_at_last(7))
+print(ll.search(2))
+print(ll.insertBefore(9, 2))

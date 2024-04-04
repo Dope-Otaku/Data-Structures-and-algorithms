f579feb7df8f0b700b578cd958b50bd6529a7939
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-04 23:26:53 +0530

    added basic structure for doubly linked list

--- Doubly Linked List/prac.py
+++ Doubly Linked List/prac.py
@@ -1,1 +1,14 @@
 #doubly linked list
+
+class dll:
+    def __init__(self, start=None):
+        self.start = start
+
+class Node:
+    def __init__(self, prev=None, next=None, item=None):
+        self.prev = prev
+        self.next = next
+        self.item = item
+
+n = Node()
+print(n)

79eab0eb81a132e84703b3542f6866eda8605f6b
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-07 19:55:37 +0530

    added the correct empty method in dll not in Node

--- Doubly Linked List/prac.py
+++ Doubly Linked List/prac.py
@@ -1,22 +1,21 @@
 #doubly linked list
 
-class dll:
-    def __init__(self, start=None):
-        self.start = start
-
-
-
-
 class Node:
     def __init__(self, prev=None, next=None, item=None):
         self.prev = prev
         self.next = next
         self.item = item
 
+class dll:
+    def __init__(self, start=None):
+        self.start = start
+
     def is_empty(self):
-        if self.prev is None:
+        if self.start is not None:
             return f"List is empty"
 
-n = Node()
+    
+
+n = dll()
 print(n)
 print(n.is_empty())

ee0b23ff439a0b7b0ef907a4ac2c2bdeb6307183
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-07 18:25:42 +0530

    added the isEmpty fn in the dll node

---
+++ Doubly Linked List/op.py
@@ -0,0 +1,17 @@
+word1 = "abc"
+word2 = "def"
+
+print(len(word1))
+# result = []
+# for i in word1:
+#         result.append(i)
+#         print(i)
+#         for j in word2:
+#             result.append(j)
+#             # result.append(i)
+#             pass
+#             print(j)
+
+
+    
+# print(result)
--- Doubly Linked List/prac.py
+++ Doubly Linked List/prac.py
@@ -4,11 +4,19 @@ class dll:
     def __init__(self, start=None):
         self.start = start
 
+
+
+
 class Node:
     def __init__(self, prev=None, next=None, item=None):
         self.prev = prev
         self.next = next
         self.item = item
 
+    def is_empty(self):
+        if self.prev is None:
+            return f"List is empty"
+
 n = Node()
 print(n)
+print(n.is_empty())

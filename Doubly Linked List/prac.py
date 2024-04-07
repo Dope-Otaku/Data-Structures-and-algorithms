7d85f2152e1e9563e8f57ed9474bb5012bcf6810
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-07 20:30:50 +0530

    added the insertAtFirst method in dll first version

--- Doubly Linked List/prac.py
+++ Doubly Linked List/prac.py
@@ -11,11 +11,26 @@ class dll:
         self.start = start
 
     def is_empty(self):
-        if self.start is not None:
+        if self.start is None:
             return f"List is empty"
-
+        else:
+            return f"List is not Empty!"
+        
+    def insertAtFirst(self, item):
+        node = Node(None, None, item)
+        temp = self.start
+        if self.is_empty():
+            self.start = node
+            print(f"added the item: {item} in node stored at {node} at first as node was empty!")
+        else:
+            while temp is not None:
+                tempNode = temp.next
+                tempNode.prev = node
+                print(f"added the item: {item} in node stored at {node} but this is betweeen an earlier node!")
+            
     
 
 n = dll()
 print(n)
+print(n.insertAtFirst(1))
 print(n.is_empty())

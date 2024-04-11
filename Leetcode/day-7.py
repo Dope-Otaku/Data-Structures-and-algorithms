061815284b9903a77a4fb8e17e5ae054d341a69e
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-11 23:41:14 +0530

    solved the logic

---
+++ Leetcode/day-7.py
@@ -0,0 +1,11 @@
+#product of array except self
+
+nums = [1,2,3,4]
+answer = []
+
+start = 0
+end = len(nums)-1
+
+for i in range(len(nums)):
+        if i == start and start < end:
+                

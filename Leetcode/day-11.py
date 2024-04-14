31b6db969698f33d5489a560163048edcdff0217
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-15 03:39:04 +0530

    solced the day-11 again

--- Leetcode/day-11.py
+++ Leetcode/day-11.py
@@ -1,24 +1,12 @@
 # is subsequence
 
-s = "axc"
-t = "ahbgdc" 
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sp = tp = 0
 
-s, t= list(s), list(t)
-for sub in t:
-    for io in s:
-        if sub == io and sub in s:
-            print(True)
-        else:
-            break
-
-        # class Solution:
-        #     def isSubsequence(self, s: str, t: str) -> bool:
-        #         sp = tp = 0
-
-        #         while sp < len(s) and tp < len(t):
-        #             if s[sp] == t[tp]:
-        #                 sp += 1
-        #             tp += 1
-                
-        #         return sp == len(s)
-    
+        while sp < len(s) and tp < len(t):
+            if s[sp] == t[tp]:
+                sp += 1
+            tp += 1
+        
+        return sp == len(s)

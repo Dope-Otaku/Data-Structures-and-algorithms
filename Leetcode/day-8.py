a9281a84412eb3f6a0cd0963e6b61cc141d46594
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-15 00:30:19 +0530

    solved again day8 by myself

--- Leetcode/day-8.py
+++ Leetcode/day-8.py
@@ -1,12 +1,26 @@
 #  Increasing Triplet Subsequence
+# class Solution:
+#     def increasingTriplet(self, nums: List[int]) -> bool:
+#         first = second = float('inf') 
+#         for n in nums: 
+#             if n <= first: 
+#                 first = n
+#             elif n <= second:
+#                 second = n
+#             else:
+#                 return True
+#         return False
+
+
+#my own logic after watching explanation
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        first = second = float('inf') 
-        for n in nums: 
-            if n <= first: 
-                first = n
-            elif n <= second:
-                second = n
+        num1, num2 = inf, inf
+        for num3 in nums:
+            if num3<= num1:
+                num1 = num3
+            elif num3<=num2:
+                num2 = num3
             else:
                 return True
         return False

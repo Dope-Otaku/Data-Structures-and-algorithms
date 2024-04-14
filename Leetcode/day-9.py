66e4ca443735029bd3b2b32b0b8cec114d75c95b
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-15 02:32:09 +0530

    solved this by myself day-9 question

--- Leetcode/day-9.py
+++ Leetcode/day-9.py
@@ -1,21 +1,50 @@
 #  String Compression
 
+# class Solution:
+#   def compress(self, chars: List[str]) -> int:
+#     ans = 0
+#     i = 0
+
+#     while i < len(chars):
+#       letter = chars[i]
+#       count = 0
+#       while i < len(chars) and chars[i] == letter:
+#         count += 1
+#         i += 1
+#       chars[ans] = letter
+#       ans += 1
+#       if count > 1:
+#         for c in str(count):
+#           chars[ans] = c
+#           ans += 1
+
+#     return ans
+
+
+# this is my own solution
 class Solution:
   def compress(self, chars: List[str]) -> int:
-    ans = 0
-    i = 0
+    nu_char = len(chars)
 
-    while i < len(chars):
-      letter = chars[i]
-      count = 0
-      while i < len(chars) and chars[i] == letter:
-        count += 1
+    if nu_char < 2:
+        return nu_char
+    i = index = 0
+    while i < nu_char:
+        count = 1
+        #chk for dupli and count it
+        while i < nu_char -1 and chars[i]==chars[i+1]:
+            count += 1
+            i += 1
+
+        chars[index] = chars[i] #call for index if next value is !=
+        index += 1
+
+        #assign the values to main list not a seperate one
+        if count > 1: #i still have a doubt here
+            for val in str(count):
+                chars[index] = val
+                index += 1
         i += 1
-      chars[ans] = letter
-      ans += 1
-      if count > 1:
-        for c in str(count):
-          chars[ans] = c
-          ans += 1
+    return index
 
-    return ans
+# print(count)

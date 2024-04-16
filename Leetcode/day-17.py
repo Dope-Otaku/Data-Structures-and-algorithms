df5817521ca59b6dea28f4b8f51e4baa2a874a6f
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-16 23:25:49 +0530

    ad

--- Leetcode/day-17.py
+++ Leetcode/day-17.py
@@ -1,5 +1,23 @@
-#linkedist solutiona
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return 
+        if head.next == None:
+            return 
 
-class sll:
-    def __init__(self, start=None):
-        self.start = start
+        slow = head
+        fast = head
+        fast = fast.next.next
+
+        while fast != None and fast.next != None:
+            fast = fast.next.next
+            slow = slow.next
+        
+        slow.next = slow.next.next
+
+        return head

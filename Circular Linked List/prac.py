1bf2b76fbe2f186296f0f486e3f641a51603a610
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-17 16:53:22 +0530

    created the isEmpty and insertatfirst function

--- Circular Linked List/prac.py
+++ Circular Linked List/prac.py
@@ -1,1 +1,28 @@
 #circular linked list
+
+class Node:
+    def __init__(self, item=None , next=None):
+        self.item = item
+        self.next = next
+class CLL:
+    def __init__(self, start=None):
+        self.start = start
+
+    def isEmpty(self):
+        if self.start == None:
+            return True
+        return False
+
+    def insertatFirst(self, data):
+        if self.isEmpty():
+            node = Node(data, self.start)
+            self.start = node
+            return f"added succesfully"
+        return f"failed to add"
+
+
+new = CLL()
+
+print(new)
+print(new.insertatFirst(9))
+print(new.isEmpty())

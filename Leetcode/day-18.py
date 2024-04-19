7563d343bd40dd973506d5d485c09754bc3cd10f
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-19 20:20:53 +0530

    solved oddeven linkedlist

--- Leetcode/day-18.py
+++ Leetcode/day-18.py
@@ -1,16 +1,16 @@
 class Solution:
     def oddEvenList(self, head):
-        
-        if not head : return head                   # [1] trivial case
-        
-        dummy_odd = odd = ListNode(0, head)         # [2] initialize dummy nodes
-        dummy_evn = evn = ListNode(0, head.next)    #     to facilitate iteration
-                
-        while odd.next and (odd := odd.next) and (evn := evn.next):
-            
-            if odd.next: odd.next = odd.next.next   # [3] skip one node for 
-            if evn.next: evn.next = evn.next.next   #     both evens and odds
-        
-        odd.next = dummy_evn.next                   # [4] append evens to odds
-        
-        return dummy_odd.next
+        if not head or not head.next : return head
+
+        odd = head
+        evenhead = even = head.next
+
+        while even and even.next:
+            odd.next = odd.next.next
+            odd = odd.next
+
+            even.next = even.next.next
+            even = even.next
+        odd.next = evenhead
+
+        return head

8ddee4e53ad02b3641013392f8056d2b787382d1
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-19 19:45:46 +0530

    solved delete the middle node question

--- Leetcode/day-17.py
+++ Leetcode/day-17.py
@@ -1,23 +1,17 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#the trick fo this question is to stop before going to middle element and then remove the next varibale reffernce from it
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head == None:
+        if head is None:
             return 
-        if head.next == None:
+        if head.next is None:
             return 
 
         slow = head
         fast = head
         fast = fast.next.next
-
         while fast != None and fast.next != None:
             fast = fast.next.next
             slow = slow.next
-        
-        slow.next = slow.next.next
 
+        slow.next = slow.next.next #here we delete the middle node
         return head

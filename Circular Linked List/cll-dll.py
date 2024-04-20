af39f0a7cc9246374fd2ab2fee5b2dc16cbb5867
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-20 23:47:41 +0530

    added the insert at last function

--- Circular Linked List/cll-dll.py
+++ Circular Linked List/cll-dll.py
@@ -24,7 +24,20 @@ class CLL:
             node.prev = self.start
             node.next = node
             return node
-    
+    def insert_At_last(self, data):
+        node = Node(data)
+        if not self.isEmpty():
+            last_node = self.start.prev
+            last_node.next = node
+            self.start.prev = node
+            node.next = self.start
+            return node
+        self.start = node.prev
+        node.next = node
+        return node
+        
+
 new = CLL()
 print(new.isEmpty())
-print(new.insert_At_first(1))
+print(new.insert_At_first(1).item)
+print(new.insert_At_last(5).item)

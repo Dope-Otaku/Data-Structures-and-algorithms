b9c5d99d0b1fab3d051398126f254ade12e8d168
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-20 20:47:55 +0530

    created a empty check functio

--- Circular Linked List/cll-dll.py
+++ Circular Linked List/cll-dll.py
@@ -1,1 +1,17 @@
 #circular doubly linked list
+
+class Node:
+    def __init__(self, item=None, prev=None, next=None):
+        self.item = item
+        self.prev= prev
+        self.next = next
+
+class CLL:
+    def __init__(self, start=None):
+        self.start = start
+
+    def isEmpty(self):
+        return self.start==None
+    
+new = CLL()
+print(new.isEmpty())

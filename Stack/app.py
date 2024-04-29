758d59e19142db7a2b34386823e3b9565f244fb7
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-04-29 15:30:02 +0530

    implementing stack using list

--- Stack/app.py
+++ Stack/app.py
@@ -1,7 +1,24 @@
-#stack code start
+#stack using list
 
 class SLL():
-    def __init__(self, start=None, head=None):
-        self.start =start
-        self.head = head
+    def __init__(self, item = None):
+        self.list = []
+        self.item = item
         
+    def is_empty(self):
+        if self.list == []:
+            return True
+
+    def push(self, item):
+        if self.is_empty():
+            self.list.append(item)
+            return f"added | list was empty"
+        self.list.append(item)
+        return f"added | list was not empty"
+
+
+
+new = SLL()
+print(new.is_empty())
+print(new.push(1))
+print(new.is_empty())

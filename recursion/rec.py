4600341884a8e66494853b7bd6c54e3fafba7a51
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-05-27 21:23:20 +0530

    added the second question of reverse

--- recursion/rec.py
+++ recursion/rec.py
@@ -2,9 +2,18 @@
 
 #q.1 write a recursion function to print first N natural numbers
 
+# def natural(n):
+#     if n > 0:
+#         natural(n-1)
+#         print(n, end=' ')
+
+# print(natural(5))
+
+#q.2 write a recursion function to print first N natural numbers in reverse order
+
 def natural(n):
     if n > 0:
-        natural(n-1)
         print(n, end=' ')
+        natural(n-1)
 
 print(natural(5))

ab4fa6529599fa54a466ea5b7cd781a979ed721f
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-05-27 18:03:18 +0530

    completed the pq with list

--- priority queue/app.py
+++ priority queue/app.py
@@ -13,9 +13,22 @@ class Pq:
         self.item.insert(index, (data, role))
         return f"added successfully"
 
+    #this function is only for testing and not included in priority queue functions! i might have done it earlier so just ignore those...
     def printal(self):
         return self.item
     
+    def is_Empty(self):
+        if self.item == []:
+            return f"empty hai"
+        return False
+
+    def pop(self):
+        if not self.is_Empty():
+            return self.item.pop(0)[0]  #can be return self.item.pop(0)
+    
+    def size(self):
+        return len(self.item)
+
 q = Pq()
 
 print(q.push(10,6))
@@ -24,4 +37,7 @@ print(q.push(30,2))
 print(q.push(40,1))
 print(q.push(50,3))
 print(q.push(60,5))
+print(q.size())
+print(q.pop())
 print(q.printal())
+print(q.size())

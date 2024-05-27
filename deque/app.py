a02296e46d5c0b1a4658f390280c726021dc1809
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-05-27 16:16:38 +0530

    added more functions such as getront andget rear and also print all for checking the deque

--- deque/app.py
+++ deque/app.py
@@ -4,6 +4,13 @@ class Deque:
     def __init__(self):
         self.item = []
 
+    def print_all(self):
+        return self.item
+    def is_empty(self):
+        if self.item == []:
+            return f"Deque is empty!"
+        return f"Deque is not empty!"
+
     def insert_front(self, data):
         self.item.insert(0, data)
         return f"inserted at front successfully!"
@@ -11,11 +18,22 @@ class Deque:
     def insert_rear(self, data):
         self.item.append(data)
         return f"inserted at rear successfully!"
+    
+    def get_rear(self):
+        return self.item[len(self.item)-1]
+    def get_front(self):
+        return self.item[0]
 
 q = Deque()
 
+
+print(q.is_empty())
 print(q.insert_front(1))
 print(q.insert_front(3))
 print(q.insert_front(4))
 print(q.insert_front(5))
 print(q.insert_rear(2))
+print(q.get_rear())
+print(q.get_front())
+print(q.print_all())
+print(q.is_empty())

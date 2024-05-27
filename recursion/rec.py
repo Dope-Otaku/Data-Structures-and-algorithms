67481afa915be4b58cf23cdc527d7081c2105eed
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-05-27 21:34:12 +0530

    added the odd number using recursion

--- recursion/rec.py
+++ recursion/rec.py
@@ -11,9 +11,18 @@
 
 #q.2 write a recursion function to print first N natural numbers in reverse order
 
-def natural(n):
-    if n > 0:
-        print(n, end=' ')
-        natural(n-1)
+# def natural(n):
+#     if n > 0:
+#         print(n, end=' ')
+#         natural(n-1)
 
-print(natural(5))
+# print(natural(5))
+
+#q.3 write a recursive fnction to print first N odd natural numbers
+
+def odd(n):
+    if n>0:
+        odd(n-1)
+        print(2*n-1, end=' ')
+
+print(odd(10))

40859702eee1bcf6fcbf402cd154e39be85e03d5
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-06-29 23:13:26 +0530

    created the first root node

--- tree/app.py
+++ tree/app.py
@@ -9,4 +9,18 @@ class Node:
 
 class Tree:
     def __init__(self, root=None):
-        self.root = root
+        self.root = root   #ref variable which will point our root node at first
+        self.val = []
+
+    def printA(self):
+        self.val.append(1)
+        return f"{self.val}"
+    
+    def insert_at_root(self, item):
+        node = Node(item)
+        self.root = node
+        return f"{node.item} at root node"
+
+new = Tree()
+print(new.printA())
+print(new.insert_at_root(2))

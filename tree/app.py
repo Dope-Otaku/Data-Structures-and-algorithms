e0e78a4c7dee0aa7b8f727f6fdb5bfe91dd856c7
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-06-30 22:22:48 +0530

    we need to remove the logic for tree

--- tree/app.py
+++ tree/app.py
@@ -11,7 +11,7 @@ class Node:
 class Tree:
     def __init__(self, root=None):
         self.root = root   #ref variable which will point our root node at first
-        self.val = []
+        # self.val = []    #we will not use a list as it will use another ds to create and hence will be much complex later on
 
     def printA(self):
         self.val.append(1)

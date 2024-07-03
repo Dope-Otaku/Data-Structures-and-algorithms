9681307253c008739b6d6618c0e680e04ffa5d25
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-07-03 23:18:51 +0530

    ok

--- tree/app.py
+++ tree/app.py
@@ -2,7 +2,7 @@
 # today we created this two different class objects
 #33:59
 #need to watch recursion vid again ig
-# ok so i have a slight de 
+# ok so i have a slight idea of how does this recursion takes place and also  
 class Node:
     def __init__(self, item=None, left=None, right=None):
         self.item = item

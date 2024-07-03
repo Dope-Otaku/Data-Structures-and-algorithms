87a41a41c1e25947209e7a59361467ee9894d932
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-07-03 23:06:59 +0530

    did it on my own

--- tree/app.py
+++ tree/app.py
@@ -14,20 +14,35 @@ class BinarySearchTree: #name can be anything
         self.root = None   #ref variable which will point our root node at first
         # self.val = []    #we will not use a list as it will use another ds to create and hence will be much complex later on
 
-    def insert(self, data):
-        self.root= self.rinsert(self.root, data)
+    # def insert(self, data):
+    #     self.root= self.rinsert(self.root, data)
     
-    def rinsert(self, root, data): #recursion function started still not able to get it
+    # def rinsert(self, root, data): #recursion function started still not able to get it
+    #     if root is None:
+    #         return Node(data)
+
+    #     if data < root.item:
+    #         root.left = self.rinsert(root.left, data) #this paert specifically
+    #         print("left")
+
+    #     elif data > root.item:
+    #         root.right = self.rinsert(root.right, data)
+    #         print("right")
+
+    #     return root
+
+
+    def insert(self, data):
+        self.root = self.rinsert(self.root, data)
+
+    def rinsert(self, root, data):
         if root is None:
             return Node(data)
-
-        if data < root.item:
-            root.left = self.rinsert(root.left, data) #this paert specifically
-            print("left")
+        elif data < root.item:
+            root.left = self.rinsert(root.left, data)
 
         elif data > root.item:
             root.right = self.rinsert(root.right, data)
-            print("right")
 
         return root
 

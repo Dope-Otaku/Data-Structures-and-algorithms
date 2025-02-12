0e455642ec6a78a63ca61688fa502b8d128b0d1b
Dope-Otaku <fakeyt909@gmail.com>
2025-02-12 23:47:45 +0530

    created the internal function at firstmost

--- Leetcode Problems/2342/prac.py
+++ Leetcode Problems/2342/prac.py
@@ -4,3 +4,26 @@
 '''
 hint - group all the numbers which have same sum
 '''
+x = [18, 43, 36, 13, 7]
+
+
+# first part completed of internal int adding
+def internalAdder(nums):
+    # return sum[int(d) for d in str(nums)]
+    # print(sum(int[d] for d in str(nums)))
+    return sum([int(d) for d in str(nums)])
+    
+
+
+
+def maxSum(x):
+    newList = []
+    i = x[0]
+
+q =str(12)
+print(internalAdder(q))
+
+
+l = 19
+p = [int(i) for i in str(l)]
+print(p)

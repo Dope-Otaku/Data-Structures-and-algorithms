9128ce2c788782db67f4d0affb8eb5a4ef4c332e
Dope-Otaku <fakeyt909@gmail.com>
2025-02-17 12:51:56 +0530

    started prac

--- recursion/rec.py
+++ recursion/rec.py
@@ -9,6 +9,21 @@
 
 # print(natural(5))
 
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(10))
+
+
+
+
+
 #q.2 write a recursion function to print first N natural numbers in reverse order
 
 # def natural(n):

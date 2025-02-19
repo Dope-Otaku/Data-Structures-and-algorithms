9a8940dcd5278f212977adb91e2df0334b431aca
Dope-Otaku <fakeyt909@gmail.com>
2025-02-19 15:21:39 +0530

    fixed a reverse logic bug i dont know what tge fuck is wrong with python

--- Doubly Linked List/new.py
+++ Doubly Linked List/new.py
@@ -8,29 +8,43 @@ class Node:
 
 
 class DLL:
-    def __init__(self):
-        self.start = None
+    def __init__(self, start=None):
+        self.start = start
 
     def is_empty(self):
         return self.start == None
+
+    def printAll(self):
+        if not self.is_empty():
+            temp = self.start
+            while temp is not None:
+                print(temp.data, end = ' ')
+                temp = temp.next
+
 
 
     def insert_first(self, data):
         newNode = Node(None,self.start, data)
         # print(newNode.data)
-        if self.is_empty():
-            self.start = newNode
-        self.start.prev = newNode
+        if not self.is_empty():
+            self.start.prev = newNode
+            print(f"added a new list with{data}")
+        self.start = newNode
+        print(f"added the list with{data}")
 
     def insert_last(self, data):
-        newNode = Node(None, None, data)
-        if not self.is_empty():
-            temp = self.start
-            while temp is not None:
-                temp = temp.next
-            temp.next = newNode
-            newNode.prev = temp
-        self.start = newNode
+        newNode = Node(data = data)
+        if self.start == None:
+            self.start = newNode
+        
+        temp = self.start
+        while temp.next is not None:
+            temp = temp.next
+        newNode.prev = temp
+        temp.next = newNode
+        print(f"added in the last list {data}")
+            
+        
 
 
 
@@ -40,5 +54,10 @@ class DLL:
 myList = DLL()
 
 print(myList.is_empty())
+print(myList.insert_first(4))
+print(myList.insert_first(3))
+print(myList.insert_first(2))
 print(myList.insert_first(1))
+print(myList.insert_last(5))
+print(myList.printAll())
 

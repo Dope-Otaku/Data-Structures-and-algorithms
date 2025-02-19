301bb8645e5065e21a58cf0f8c2f4bfe0794cfc4
Dope-Otaku <fakeyt909@gmail.com>
2025-02-19 14:10:45 +0530

    basic backbone of dll added

---
+++ Doubly Linked List/new.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, prev=None, next=None, data=None):
+        self.prev = prev
+        self.next = next
+        self.data = data
+
+
+
+
+class DLL:
+    def __init__(self):
+        self.start = start
+
+    

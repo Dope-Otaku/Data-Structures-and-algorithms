cf0f3eafda9c1c93635235d7325b186064476b3f
Dope-Otaku <fakeyt909@gmail.com>
2025-02-21 17:10:47 +0530

    added backbone for cll

--- Circular Linked List/cll-sll-prac.py
+++ Circular Linked List/cll-sll-prac.py
@@ -1,3 +1,12 @@
 '''
 in this we will practise circular singly linked list
 '''
+class Node:
+    def __init__(self, prev=None, data=None, next= None):
+        self.prev = prev
+        self.data = data
+        self.next = next
+
+class CLL:
+    def __init__(self, start=None):
+        self.start = start

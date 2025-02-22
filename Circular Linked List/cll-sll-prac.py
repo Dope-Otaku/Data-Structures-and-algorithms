bb40f9aa4741e6841d0006baa05a05e341392158
Dope-Otaku <fakeyt909@gmail.com>
2025-02-23 01:50:17 +0530

    fixed insert at last function too

--- Circular Linked List/cll-sll-prac.py
+++ Circular Linked List/cll-sll-prac.py
@@ -1,5 +1,6 @@
 '''
-in this we will practise circular Doubly linked list
+in this we will practise circular Doubly linked list and imp point to remember we will start
+traversing from last which will decrease the time to traverse last node and not start node
 '''
 class Node:
     def __init__(self, data=None, next= None):
@@ -36,16 +37,15 @@ class CLL:
             newNode.next = self.last.next
             self.last.next = newNode
 
-    def insertLast(self, item): #this function is obsolete
-        newNode = Node(data = item, next= self.last)
-        temp = self.last
-        # if self.isEmpty():
-        #     self.last = newNode
-        while temp is not None:
-            if temp.next == self.last:
-                temp.next = newNode
-                print("hi")
-            temp = temp.next
+    def insertLast(self, item): 
+        newNode = Node(data = item)
+        if self.last == None:
+            self.last = newNode
+        else:
+            newNode.next = self.last.next
+            self.last.next = newNode
+            self.last = newNode
+        
 
 
 
@@ -74,9 +74,10 @@ print(myList.insertFront(1))
 print(myList.insertFront(2))
 print(myList.insertFront(3))
 print(myList.insertFront(4))
-print(myList.isEmpty())
+print(myList.insertLast(5))
+print(myList.insertLast(6))
+# print(myList.isEmpty())
 print(myList.travserse())
-# print(myList.insertLast(3))
 
 
 

42cf6cce781389ca02a95bb6978792bdeea961db
Dope-Otaku <fakeyt909@gmail.com>
2025-03-01 01:40:02 +0530

    tested and fixed the remaining method

--- priority queue/prac.py
+++ priority queue/prac.py
@@ -19,11 +19,14 @@ class PQ:
     def pop(self):
         if self.item == []:
             raise EOFError("PQ has No elements")
-        self.item.pop(0)[0]
+        return self.item.pop(0)[0]
 
     def is_Empty(self):
         return len(self.item) == 0
 
+
+    def size(self):
+        return len(self.item)
 
 
 
@@ -32,4 +35,8 @@ class PQ:
 
 myList = PQ()
 
+print(myList.push(2, 10))
+print(myList.push(1, 9))
+print(myList.push(3, 11))
 print(myList.pop())
+print(myList.size())

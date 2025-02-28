e8c3aad6a8c076eec0a33670340a609f93b03fca
Dope-Otaku <fakeyt909@gmail.com>
2025-03-01 01:21:35 +0530

    prirority queue added using push method

--- priority queue/prac.py
+++ priority queue/prac.py
@@ -9,3 +9,9 @@ priority queue
 class PQ:
     def __init__(self):
         self.item = []
+
+    def push(self, data, priority):
+        index = 0
+        while index<len(self.item) and self.item[index][1]<=priority:
+            index+=1
+        self.item.insert(index, (data, priority))

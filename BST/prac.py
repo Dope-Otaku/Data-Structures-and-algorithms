02bb268478b0ba96284d755bb55a53d5ecf4c394
Dope-Otaku <fakeyt909@gmail.com>
2025-03-01 23:19:37 +0530

    bst readme updated

--- BST/prac.py
+++ BST/prac.py
@@ -3,7 +3,7 @@ here i will jusst practise all the questions asked !
 
 BST -
 Completely filled tree
-almost complete binary tree
+almost complete binary tree -> only the last level nodes are unfilled and that too of left side
 strict binary tree -> the node should either have 0 or two attached nodes
 
 '''

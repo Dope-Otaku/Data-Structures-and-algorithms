01024514ff6128d841f1cc6a33f5d2e2a3c89bd5
Dope-Otaku <fakeyt909@gmail.com>
2025-03-01 22:45:26 +0530

    sbt read

--- BST/prac.py
+++ BST/prac.py
@@ -4,6 +4,6 @@ here i will jusst practise all the questions asked !
 BST -
 Completely filled tree
 almost complete binary tree
-strict binary tree
+strict binary tree -> the node should either have 0 or two attached nodes
 
 '''

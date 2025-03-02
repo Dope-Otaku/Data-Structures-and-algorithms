195d4a901525f2ec055ea12fae283c7b15534ad6
Dope-Otaku <fakeyt909@gmail.com>
2025-03-02 22:59:20 +0530

    removed all earlier insertion code

--- BST/BST_p1.py
+++ BST/BST_p1.py
@@ -13,28 +13,8 @@ class BST:
     def isEmpty(self):
         return self.root == None
 
-    def insertatroot(self, data):
-        newNode = Node(item=data)
-        if not self.isEmpty():
-            newNode.right = self.root.right
-            newNode.left = self.root.left
-            self.root.left, self.root.right = None, None
-            self.root = newNode
-            return f"changed the root node to {data}"
-        self.root = newNode
-        return f"inserted root node {data}"
-
-    
-
-    def insertatleft(self, data):
-        if self.root.left == None:
-            newNode = Node(item=data)
-            self.root.left = newNode
-
-    def insertatright(self, data):
-        if self.root.right == None:
-            newNode = Node(item=data)
-            self.root.right = newNode
+    def insertion(self, data):
+        pass
 
 
 

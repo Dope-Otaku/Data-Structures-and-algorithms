fe0f129d926a9b281f5cdb2e0207d864b09d5024
Dope-Otaku <fakeyt909@gmail.com>
2025-03-02 23:50:19 +0530

    adding recursive approach here is getting hard

--- BST/BST_p1.py
+++ BST/BST_p1.py
@@ -17,7 +17,19 @@ class BST:
         newNode = Node(item=data)
         if self.root == None:
             self.root = newNode
-        
+        else:
+            temp = self.root
+            while temp is not None:
+                if self.root.item > data:
+                    if self.root.item == None:
+                        self.root.left = newNode
+                    else:
+                        temp = self.root.left
+                        
+                    if self.root.item
+                elif self.root.item < data:
+                    self.root.right = newNode
+            
 
 
 

bb4929498affac97cd84824904994d3f2ffdd5c7
Dope-Otaku <fakeyt909@gmail.com>
2025-03-06 03:23:59 +0530

    added driver code for testing

--- Graph/graph.py
+++ Graph/graph.py
@@ -31,3 +31,19 @@ class Graph:
     def print_adj_matrix(self):
         for row_list in self.adj_matrix:
             print(" ".join(map(str, row_list)))
+
+
+
+#driver code
+
+myList = Graph(5)
+
+print(myList.add_edge(2,4))
+print(myList.add_edge(1,4))
+print(myList.add_edge(2,2))
+print(myList.add_edge(0,4))
+print(myList.add_edge(0,0))
+print(myList.has_edge(0,0))
+print(myList.print_adj_matrix())
+print(myList.remove_edge(0,0))
+print(myList.print_adj_matrix())

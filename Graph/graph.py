d8ce6e84f42b06c065b2f0f7d650bafdebb2a9c7
Dope-Otaku <fakeyt909@gmail.com>
2025-03-06 03:55:45 +0530

    fixed remove edge method

--- Graph/graph.py
+++ Graph/graph.py
@@ -11,13 +11,16 @@ class Graph:
         else:
             return f"invalid vertex"
 
-    def remove_edge(self, u, v, weight=0):
+    def remove_edge(self, u, v):
         if 0<=u<self.vertex_count and 0<=v<self.vertex_count:
-            self.adj_list[u].append((v, weight))
-            self.adj_list[v].append((u, weight))
+            self.adj_list[u] = [(vertex, weight) for vertex, weight in self.adj_list[u] if vertex!=v]
+            self.adj_list[v] = [(vertex, weight) for vertex, weight in self.adj_list[v] if vertex!=u]
         else:
             return f"invalid vertex"
 
     def has_edge(self, u, v):
         if 0<=u<self.vertex_count and 0<=v<self.vertex_count:
             return any(vertex == v for vertex, x in self.adj_list[u])
+
+    def print_adj_list(self):
+        pass

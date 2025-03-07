ad9097038361e8436f74fc83d0c88b9b60dda9a8
Dope-Otaku <fakeyt909@gmail.com>
2025-03-07 22:36:27 +0530

    visited neightbout pop out

--- BFS/prac.py
+++ BFS/prac.py
@@ -19,3 +19,7 @@ def bfs(visited, graph, node):
     while queue: #creating a loop to visit each node
         m = queue.pop(0)
         print(m, end=" ")
+
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)

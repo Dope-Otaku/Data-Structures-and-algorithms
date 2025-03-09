21b1c79f171530b4e7c193e941b8046d428e75fc
Dope-Otaku <fakeyt909@gmail.com>
2025-03-09 23:30:37 +0530

    practised bfs

---
+++ BFS/prac2.py
@@ -0,0 +1,38 @@
+graph = {
+    '9': ['7'],
+    '7': [ '5'],
+    '5': ['4'],
+    '4': ['3', '2'],
+    '3': [],
+    '2': []
+}
+# graph = {
+#     '5' : ['3', '7'],
+#     '3' : ['2', '4'],
+#     '7' : ['8'],
+#     '2' : [],
+#     '4' : ['8'],
+#     '8' : []
+# }
+
+
+visited = []
+queue = []
+
+def bfs(visited, graph, node):
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+        m = queue.pop(0)
+        print(m, end=" ")
+
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+
+
+bfs(visited, graph, '9')
+# bfs(visited, graph, '5')

a81860de38a933c0b37f90e871c26e6e85ef15a0
Dope-Otaku <fakeyt909@gmail.com>
2025-03-17 23:15:10 +0530

    created this while loop just to handle comparisons

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,3 +1,9 @@
 t = [50, 20, 37, 91, 64, 18, 43, 59, 72, 81]
 
 compared_value = len(t) -1 #this many times the operation will run
+count = 0
+
+
+while count >=0 and count <= compared_value:
+    print(t[count])
+    count += 1

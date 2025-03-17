3df88f9709b977f5c1d467e43aba47bcd089b3ec
Dope-Otaku <fakeyt909@gmail.com>
2025-03-17 22:50:08 +0530

    added info about bubble sort

--- sorting/info.py
+++ sorting/info.py
@@ -11,3 +11,11 @@
 7. heap sort
 
 '''
+
+'''
+bubble sort
+
+in this sort method we usually compare all the comaprisons till the comparisons are empty, 
+meaning we check pair of values and if lower value is present we swap them.
+
+'''

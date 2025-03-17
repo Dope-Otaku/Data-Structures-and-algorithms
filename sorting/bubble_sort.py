842ec69059b7d6b9751f06b0ad84b209f26e92c1
Dope-Otaku <fakeyt909@gmail.com>
2025-03-17 23:22:16 +0530

    created the whole bubble sort code with error but we can look forward to creating a new error free sort algo

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -2,8 +2,19 @@ t = [50, 20, 37, 91, 64, 18, 43, 59, 72, 81]
 
 compared_value = len(t) -1 #this many times the operation will run
 count = 0
+left = 0
+right = 1
 
 
 while count >=0 and count <= compared_value:
-    print(t[count])
+    if t[left] > t[right]:
+        t[left], t[right] = t[right], t[left]
+        left += 1
+        right += 1
+    elif t[left] < t[right]:
+        left += 1
+        right += 1
     count += 1
+    
+
+print(t)

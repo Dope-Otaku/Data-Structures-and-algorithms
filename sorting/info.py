c8165934ed3c52052deedd39255cefc1c8aa5192
Dope-Otaku <fakeyt909@gmail.com>
2025-03-17 22:35:29 +0530

    added sorting algo mainly used

--- sorting/info.py
+++ sorting/info.py
@@ -1,1 +1,13 @@
 # arranging data elements in some logical order is called sorting. 
+
+# there are various soritng algorithms some of the major ones are:
+'''
+1. bubble sort
+2. modified bubble sort
+3. selection sort
+4. insertion sort
+5. quick sort
+6. merge sort
+7. heap sort
+
+'''

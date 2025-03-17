9d88cf92b8dc5a635479b553897cbd4285713f1e
Dope-Otaku <fakeyt909@gmail.com>
2025-03-17 23:06:02 +0530

    added info about insertion sort

--- sorting/info.py
+++ sorting/info.py
@@ -37,3 +37,12 @@ selection sort
 in this sort method , it usually works as a human brain we check the lowest value and swap the 0th element with
 it first then we check the second lowest value and update the 1st index value, like that we sort the list
 '''
+
+
+'''
+insertion sort
+
+in this sort method , we consider the first element as always small and by holding on to that we start checking other values 
+whether that is mall or greater if greater comes then we store that value and start comparing the remaining with the stored values until the list is stored!
+
+'''

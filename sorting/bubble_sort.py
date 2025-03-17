0b63d8c214e444750f7ae29cd65566b016703c3b
Dope-Otaku <fakeyt909@gmail.com>
2025-03-17 23:25:59 +0530

    changed the list with small values

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,13 +1,14 @@
-t = [50, 20, 37, 91, 64, 18, 43, 59, 72, 81]
+t = [24, 58, 11, 67, 92, 43]
 
-compared_value = len(t) -1 #this many times the operation will run
+compared_value = len(t)  #this many times the operation will run
 count = 0
 left = 0
 right = 1
 
 
-while count >=0 and count <= compared_value:
+while count >=0 and count < compared_value:
     if t[left] > t[right]:
+        print(f"left:{t[left]}, right:{t[right]}")
         t[left], t[right] = t[right], t[left]
         left += 1
         right += 1

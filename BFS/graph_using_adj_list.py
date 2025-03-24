27c43dd1df9c00403c204693693851e157b65178
Souvik_Halder <112975484+Dope-Otaku@users.noreply.github.com>
2025-03-24 22:56:53 +0530

    Update graph_using_adj_list.py

--- BFS/graph_using_adj_list.py
+++ BFS/graph_using_adj_list.py
@@ -2,6 +2,7 @@ class Graph:
     def __init__(self, vno):
         self.vertex_count = vno
         self.adj_list = {v:[] for v in range(vno)}
+#hello this is a remp function
 
 
     def add_edge(self, u, v, weight=1):

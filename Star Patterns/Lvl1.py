303b98124daeadc878c01066c967c5bf32f010f2
Dope-Otaku <fakeyt909@gmail.com>
2025-09-10 21:06:45 +0530

    completed level1 star pattern

--- Star Patterns/Lvl1.py
+++ Star Patterns/Lvl1.py
@@ -9,3 +9,12 @@
 this is level 1 and we will try to replicate this!
 '''
 
+n = 4
+
+for i in range(n):
+    for j in range(n):
+        # j+=1
+        print("*", end="")
+    print(" ") #you can also use this because it is the set to default like this only print()
+
+

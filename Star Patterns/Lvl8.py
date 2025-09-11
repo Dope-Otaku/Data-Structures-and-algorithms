6a93323a2b5bf32a67113a0cbe7e2284d487ee0d
Dope-Otaku <fakeyt909@gmail.com>
2025-09-11 23:12:46 +0530

    now in this pattern we are having issues with starts we have resolved the space issue

---
+++ Star Patterns/Lvl8.py
@@ -0,0 +1,19 @@
+'''
+*********
+ *******
+  *****
+   ***
+    *
+
+we need to implement this in our code
+'''
+
+
+n = 5 
+
+for i in range(n):
+    for j in range(0, i):
+        print(" ", end="")
+    for j in range(n-i, 0, -1):
+        print("*", end="")
+    print()

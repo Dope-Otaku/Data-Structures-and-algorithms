3284df946ed623c1165d8ec0cc479b3384449455
Dope-Otaku <fakeyt909@gmail.com>
2025-09-11 22:34:05 +0530

    finally solved lvl7

--- Star Patterns/Lvl7.py
+++ Star Patterns/Lvl7.py
@@ -13,14 +13,11 @@ n = 5
 
 for i in range(n):
     #space
-    for j in range(0, n-i, -1):
-        print(" ")
-        print("space 1")
+    for j in range(0, n-i):
+        print(" ", end="")
+        # print("space 1")
     #star
     for j in range(2*i+1):
         print("*", end="")
-    #space
-    for j in range(0, n-i, -1):
-        print(" ")
-        print("space 2")
+
     print()

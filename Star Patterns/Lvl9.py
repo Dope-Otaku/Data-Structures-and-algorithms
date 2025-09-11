7223d577ab312f79a6fbd0fd74cca6052007dc55
Dope-Otaku <fakeyt909@gmail.com>
2025-09-12 00:29:36 +0530

    finnalyy after pasting the prev codes only i got the solution but it wasted a lot of time

--- Star Patterns/Lvl9.py
+++ Star Patterns/Lvl9.py
@@ -15,41 +15,23 @@ we need to implement this in code
 
 '''
 
-n = 10
-m = int(n/2)
-
+n = 5
 
 for i in range(n):
-    for j in range(m):
-        for a in range(0, m-i):
-            print(" ", end="")
-            # print("space 1")
-        #star
-        for b in range(2*j+1):
-            print("*", end="")
-    print()
-    for k in range(m):
-        for c in range(0, k):
-            print(" ", end="")
-        for d in range(m-2*k, 0, -1):
-            print("*", end="")
+    #space
+    for j in range(0, n-i-1):
+        print(" ", end="")
+        # print("space 1")
+    #star
+    for j in range(2*i+1):
+        print("*", end="")
     print()
 
+# n = 9 
 
-
-
-
-
-# for i in range(n):
-#     for j in range(0, i):
-#         print(" ", end="")
-#     for j in range(n-2*i, 0, -1):
-#         print("*", end="")
-#     print()
-
-# for k in range(n):
-#     for l in range(0, i):
-#         print(" ", end="")
-#     for l in range(n-2*i, 0, -1):
-#         print("*", end="")
-#     print()
+for i in range(n):
+    for j in range(0, i):
+        print(" ", end="")
+    for j in range(2*(n-i)-1):
+        print("*", end="")
+    print()

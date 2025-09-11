b1f4f850b992db51feed7f6cec8bb67b39c3e4b0
Dope-Otaku <fakeyt909@gmail.com>
2025-09-11 23:22:40 +0530

    finished lvl8 it took more time but i just had to find the pattern and it is solved

--- Star Patterns/Lvl8.py
+++ Star Patterns/Lvl8.py
@@ -9,11 +9,11 @@ we need to implement this in our code
 '''
 
 
-n = 5 
+n = 9 
 
 for i in range(n):
     for j in range(0, i):
         print(" ", end="")
-    for j in range(n-i, 0, -1):
+    for j in range(n-2*i, 0, -1):
         print("*", end="")
     print()

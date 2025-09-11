4609ae493c0451e7d5634ec8b065f7d79832f5b2
Dope-Otaku <fakeyt909@gmail.com>
2025-09-11 22:25:54 +0530

    we have addded debug on spaces since we are having problem in that only

--- Star Patterns/Lvl7.py
+++ Star Patterns/Lvl7.py
@@ -14,11 +14,13 @@ n = 5
 for i in range(n):
     #space
     for j in range(0, n-i, -1):
-        print(" ", end="")
+        print(" ")
+        print("space 1")
     #star
-    for j in range(2*n+1):
+    for j in range(2*i+1):
         print("*", end="")
     #space
     for j in range(0, n-i, -1):
-        print(" ", end="")
+        print(" ")
+        print("space 2")
     print()

6465b31eeed213bfffd42ba28c1600c71652c196
Dope-Otaku <fakeyt909@gmail.com>
2025-09-12 13:32:15 +0530

    done with the simple solution

--- Star Patterns/Lvl14.py
+++ Star Patterns/Lvl14.py
@@ -11,9 +11,10 @@ we need to implement this
 '''
 
 n =5
-ase = ord('A')
+# ase = ord('A')
 
 for i in range(1, n+1):
+    ase = ord('A')
     for j in range(i):
         print(chr(ase), end="")
         ase = ase+1

c805658405b62c09ab51014b35f20515aacb7b50
Dope-Otaku <fakeyt909@gmail.com>
2025-09-12 13:30:33 +0530

    although iwas trying to solve a different thing similar to this but solved the next question but still it was cool will deploy in second commit

---
+++ Star Patterns/Lvl14.py
@@ -0,0 +1,21 @@
+'''
+
+
+a
+ab
+abc
+abcd
+abcde
+
+we need to implement this
+'''
+
+n =5
+ase = ord('A')
+
+for i in range(1, n+1):
+    for j in range(i):
+        print(chr(ase), end="")
+        ase = ase+1
+        
+    print()

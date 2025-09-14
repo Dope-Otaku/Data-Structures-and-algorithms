3d7445cd01a0b2bd4fee2e4c6a2960b64452f09e
Dope-Otaku <fakeyt909@gmail.com>
2025-09-14 22:38:11 +0530

    trying to solve lvl18

---
+++ Star Patterns/Lvl18.py
@@ -0,0 +1,20 @@
+'''
+   A
+  ABA
+ ABCBA
+ABCDCBA
+
+we need to implement this
+
+'''
+
+n = 5
+
+for i in range(n):
+    # for spaces
+    for j in range(n-i):
+        print(" ", end="")
+    
+    #for content after space
+    for j in range(2*i+1):
+        

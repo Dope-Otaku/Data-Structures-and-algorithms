f36b4411fa819637e8dd680252aeee7513cfaccd
Dope-Otaku <fakeyt909@gmail.com>
2026-01-02 22:44:10 +0530

    added second mistake i did today

--- count digits/bruteApproach.py
+++ count digits/bruteApproach.py
@@ -1,11 +1,16 @@
 # Given an integer N, return the number of digits in N.
 
 
-n = '4578'
+n = 4578
 
 #first mistake len() function is only available for arrray and string data type
+#second mistake learned that int value mai loop nahi laga sakte
 a = n
-print(len(a))
+b= []
+for i in range(0,5):
+    b.append(n[i])
+    i+=1
+print(b)
 
 # m = n
 # count = 0

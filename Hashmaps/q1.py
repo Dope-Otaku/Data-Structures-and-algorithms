c44e4c0788ea7d440a76860cf0a25681196d1a52
Souvik Halder <fakeyt909@gmail.com>
2026-03-06 00:23:50 +0530

    trying two sum question in hashmap style

---
+++ Hashmaps/q1.py
@@ -0,0 +1,15 @@
+'''
+two sum question:
+
+given an array of integers nums and an integer target,
+return the indices of the two numbers such that they
+add up to target. you may assume that each input would 
+have exactly one solution and you may not use the same element twice 
+
+example: 
+
+input: nums = [2,7,11,15], target = 9
+output : [0,1]
+explanation: because nums[0] + nums[1] == 9, we return [0,1]
+
+'''

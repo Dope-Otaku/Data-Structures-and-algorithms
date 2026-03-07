3277c58c349dff0839948da11b4bad369d516eb8
Souvik Halder <fakeyt909@gmail.com>
2026-03-07 11:30:12 +0530

    added sliding window new subarray sum question

---
+++ sliding window/prac.py
@@ -0,0 +1,9 @@
+'''
+Fixed size sliding window implementation in Python.
+
+given an array nums consisted  of only non-negative integers, find the largest
+sum among all subarrays of length k in nums
+
+for example: nums = [1,2,3,7,4,1], k =3, output = 14 ,[3,4,7]
+
+'''

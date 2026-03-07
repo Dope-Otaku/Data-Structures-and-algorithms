bbf5d5010d18dd967460a927aa126f6edffafc54
Souvik Halder <fakeyt909@gmail.com>
2026-03-07 11:49:11 +0530

    added my logic but now stuck because can't print the window values

--- sliding window/prac.py
+++ sliding window/prac.py
@@ -8,12 +8,19 @@ for example: nums = [1,2,3,7,4,1], k =3, output = 14 ,[3,4,7]
 
 '''
 
+nums = [1,2,3,7,4,1]
+k =3
+
 def subarray_sum_fixed(input, window_size):
     ans = window = input[0:window_size]
+    highV = []
     for right in range(window_size, len(input)):
         left = right - window_size
         window.remove(input[left])
         window.append(input[right])
-        ans = max(ans, window)
+        highV.append(sum(window))
+
+        ans = max(highV)
     return ans
 
+print(subarray_sum_fixed(nums, k))

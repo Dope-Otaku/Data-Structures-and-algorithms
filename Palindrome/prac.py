9aeff49aa6170274741f149866b36ba0c27b4e53
Souvik Halder <fakeyt909@gmail.com>
2026-03-07 10:40:53 +0530

    started learning two-pointers

---
+++ Palindrome/prac.py
@@ -0,0 +1,14 @@
+'''
+Valid Palindrome
+Easy
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+Example:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: After removing non-alphanumeric characters, it l
+Input: s = "race a car"
+Output: false
+Explanation: After removing non-alphanumeric characters, it l
+
+
+'''
